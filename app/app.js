eee860ead608d5a248e8eb3541976c23552f41c1
Krzysztof <krzysztof_mikuta@interia.pl>
2015-05-22 00:13:45 +0200

    JShint analysis added

--- app/app.js
+++ app/app.js
@@ -1,59 +1,61 @@
 (function() {
-    var app = angular.module("SknPageApp", []);
+    'use strict';
+
+    var app = angular.module('SknPageApp', []);
 
     app.directive('pageHome', function() {
         return {
             restrict: 'E',
             templateUrl: 'resources/templates/home.html',
             controller: function() {
-                this.message = "This is a Home Content";
+                this.message = 'This is a Home Content';
             },
             controllerAs: 'homeCtrl'
-        }
+        };
     });
 
-    app.directive("pageAbout", function() {
+    app.directive('pageAbout', function() {
         return {
             restrict: 'E',
             templateUrl: 'resources/templates/about.html',
             controller: function() {
-                this.message = "This is an About Content";
+                this.message = 'This is an About Content';
             },
             controllerAs: 'aboutCtrl'
-        }
+        };
     });
 
-    app.directive("pageProjects", function() {
+    app.directive('pageProjects', function() {
         return {
             restrict: 'E',
             templateUrl: 'resources/templates/projects.html',
             controller: function() {
-                this.message = "This is a Projects Content";
+                this.message = 'This is a Projects Content';
             },
             controllerAs: 'projectsCtrl'
-        }
+        };
     });
 
-    app.directive("pageGallery", function() {
+    app.directive('pageGallery', function() {
         return {
             restrict: 'E',
             templateUrl: 'resources/templates/gallery.html',
             controller: function() {
-                this.message = "This is a Gallery Content";
+                this.message = 'This is a Gallery Content';
             },
             controllerAs: 'galleryCtrl'
-        }
+        };
     });
 
-    app.directive("pageContact", function() {
+    app.directive('pageContact', function() {
         return {
             restrict: 'E',
             templateUrl: 'resources/templates/contact.html',
             controller: function() {
-                this.message = "This is a Contact Content";
+                this.message = 'This is a Contact Content';
             },
             controllerAs: 'contactCtrl'
-        }
+        };
     });
 
 })();
